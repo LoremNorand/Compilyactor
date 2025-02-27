02e60500faea0b007d52098264787b5b73438d1c
LoremNorand <lorem.norand@gmail.com>
2025-02-27 11:32:12 +0300

    @LoremNorand [Global]: add <SplashBlock>

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 using Compilyactor;
-using Compilyactor.Code;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton(SplashAggregator.Instance);
-
+builder.Services.AddHttpClient();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
